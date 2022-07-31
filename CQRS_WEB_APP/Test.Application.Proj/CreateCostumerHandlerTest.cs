8ff661a4b19bc6f94512c6c356f48cbe0f0ec416
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-07-31 20:10:05 -0300

    starting tests

--- CQRS_WEB_APP/Test.Application.Proj/CreateCostumerHandlerTest.cs
+++ CQRS_WEB_APP/Test.Application.Proj/CreateCostumerHandlerTest.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿using Application.Features.CustomerType.Handlers.Query;
+using Application.Features.CustomerType.Request.Queries;
+using Application.Persistence.Contracts;
+using Application.Profiles;
+using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Test.Application.Proj.Mocks;
 
 namespace Test.Application.Proj
 {
-    
+
     public class CreateCostumerHandlerTest
     {
-        [Fact]
-        public void CreateCostumer()
-        {
+        private readonly IMapper _maper;
+        private readonly Mock<ICostumerRepository> _repoMock;
 
+        public CreateCostumerHandlerTest()
+        {
+            _repoMock = CostumerRepositoryMock.GetCostumerRepository();
+
+            var mapperConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            _maper = mapperConfig.CreateMapper();
+        }
+        [Fact]
+        public async void GetCostumerList()
+        {
+            var handler = new CostumerListRequestHandler(_repoMock.Object,_maper);
+
+            var result = await handler.Handle(new CostumerListRequest(),CancellationToken.None);
+
+            result.Count.ShouldBeGreaterThan(0);
         }
 
     }
---
+++ CQRS_WEB_APP/Test.Application.Proj/Mocks/CostumerRepositoryMock.cs
@@ -0,0 +1,48 @@
+﻿using Application.Persistence.Contracts;
+using Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Application.Proj.Mocks
+{
+    public static class CostumerRepositoryMock
+    {
+        public static Mock<ICostumerRepository> GetCostumerRepository()
+        {
+            var costumers = new List<Costumer>()
+            {
+                new Costumer()
+                {
+                    BirthDate = DateTime.Parse("1997-12-21"),
+                    Email = "dalmasiof@gmail.com",
+                    Id = 1,
+                    Name = "Dalmasio"
+                },
+                new Costumer()
+                {
+                    BirthDate = DateTime.Parse("2007-05-13"),
+                    Email = "joaosao291@gmail.com",
+                    Id = 2,
+                    Name = "Joao Pedro"
+                },
+            };
+
+            var mockRepo = new Mock<ICostumerRepository>();
+            mockRepo.Setup(
+                repo => repo.GetListAsync()
+            ).ReturnsAsync(costumers);
+
+            mockRepo.Setup(
+                repo => repo.Create(It.IsAny<Costumer>())
+            ).ReturnsAsync(It.IsAny<Costumer>());
+
+            return mockRepo;
+
+
+        }
+    }
+}
